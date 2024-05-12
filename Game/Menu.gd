9f1677d48cf6fa8f4b40b661874fa645bcbc6e8e
Dios-Fer <115993932+Dios-Fer@users.noreply.github.com>
2024-05-12 21:19:32 +0200

    V0.2
    He realizado una versión inicial y verde del inicio priorizando la sección con la que empecé (la base de datos)

---
+++ Game/Menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+# Funcion llamada al inicio
+func _ready():
+	pass # Replace with function body.
+
+
+# Funcion de actualizacion
+func _process(delta):
+	pass
+
+# Actual boton de cerrar sesion
+func _on_cerrar_pressed():
+	Firebase.Auth.logout()
+	get_tree().change_scene_to_file("res://Autentication.tscn")
+
+
+# Boton para ir a la pantalla del perfil
+func _on_perfil_pressed():
+	#get_tree().change_scene_to_file("res://Perfil.tscn")
