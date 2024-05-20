395faa13957ff29f3f1cb4b5c20d1083706d4fda
Dios-Fer <115993932+Dios-Fer@users.noreply.github.com>
2024-05-20 21:24:41 +0200

    V0.8
    Sigo avanzando con la creación de la partida obteniendo algo de progreso en la movilidad de piezas (ahora mismo botones) y sigo pensando y probando como realizar los movimientos y comprobaciones.

--- Game/Casilla.gd
+++ Game/Casilla.gd
@@ -4,15 +4,21 @@ extends Node2D
 @onready var skinFicha = ""
 
 
+func init(newID_CASILLA, newskinFicha):
+	ID_CASILLA = newID_CASILLA
+	skinFicha = newskinFicha
+	var ruta = "res://Sprites/" + skinFicha
+	$Sprite2D.texture=ResourceLoader.load(ruta)
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var ruta = "res://Sprites/" + "Casilla2.png"
-	print(ruta)
-	$Sprite2D.texture=ResourceLoader.load(ruta)
+	pass
 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
--- Game/Game.gd
+++ Game/Game.gd
@@ -1,12 +1,27 @@
 extends Control
+@onready var button = $Background/Tablero/HBoxContainer/Casilla8A/Button
+
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Background/Tablero/HBoxContainer/Casilla1A.skinFicha = "Casilla2.png"
-	$Background/Tablero/HBoxContainer/Casilla1A.ID_CASILLA = "1A"
+	$Background/Tablero/HBoxContainer/Casilla8A.init("8A", "Casilla2.png")
+	$Background/Tablero/HBoxContainer/Casilla8B.init("8B", "Casilla1.png")
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+
+
+
+func _on_button_pressed():
+	print("inicial:"+button.get_parent().ID_CASILLA)
+	button.get_parent().remove_child(button)
+	$Background/Tablero/HBoxContainer/Casilla8B.add_child(button)
+	button.position = Vector2(0, 0)
+	print("final:"+button.get_parent().ID_CASILLA)
